c1fdc06a5434d68f4b43d4112e3671c466668a86
jonasrost <rojonas@student.chalmers.se>
2020-11-24 17:52:06 +0100

    Initilized

---
+++ javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Car"},{"p":"<Unnamed>","l":"Movable"},{"p":"<Unnamed>","l":"Saab95"},{"p":"<Unnamed>","l":"Tester"},{"p":"<Unnamed>","l":"Volvo240"}];updateSearchResults();
